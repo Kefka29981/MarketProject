0bf3900e4323fa6b1d10e61e791a1a1c87b60533
Kefka29981 <Kefka29981@gmail.com>
2022-09-08 19:18:29 +0300

    Masta

--- Scripts/Common/Interfaces/ISelectable.cs
+++ Scripts/Common/Interfaces/ISelectable.cs
@@ -8,4 +8,10 @@ public interface ISelectable: IPointerDownHandler
     //methods
     void Select();
     void Unselect();
+
+    void IPointerDownHandler.OnPointerDown(UnityEngine.EventSystems.PointerEventData eventData)
+    {
+        //select the object
+        Select();
+    }
 }
